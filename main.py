4d36d728fa23b7f8ae2af34ef28314707226e682
lachlan <lachlang93@gmail.com>
2022-04-06 02:00:25 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Server Side
+# Backend
+# django-admin startproject storefront .
+# python manage.py runserver #port
+# Settings >> Installed apps >>
+# admin, auth, content, sessions, messages, staticfiles. provides functionality.
+
+# Python manage.py startapp
+# models > tests > views >
+
+
+# Register new app in settings
+
+# What we call a 'view' in django is a template in other frameworks.
+
+# Modular > can replace built in templating engine.
+
+# Dont really use Django for Templating, but to build APIs that return data.
+
+# How to debug using jango & VS code
